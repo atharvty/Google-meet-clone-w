24263b1ea589a328b0c6b7962a1f1f43d759db4e
atharvty <73381314+atharvty@users.noreply.github.com>
2022-06-27 23:18:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Routes = require("./app/routes");
+
+const port = process.env.PORT;
+const app = express();
+const server = http.createServer(app);
+
+
+app.use([
+    cors(),
+    bodyParser.json(),
+    bodyParser.urlencoded({extended: false}),
+    Routes,
+
+])
+
+
+const io = (module.exports.io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+}));
+const socketManager = require("./app/socketManager");
+io.on("connection", socketManager);
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})
+
+
+
